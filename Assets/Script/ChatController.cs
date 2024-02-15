d1fad7f93e73f719c9bc448461445381ceea6790
dmstlf1104 <75960436+dmstlf1104@users.noreply.github.com>
2024-02-15 09:24:41 +0900

    인트로 추가

--- Assets/Script/ChatController.cs
+++ Assets/Script/ChatController.cs
@@ -59,11 +59,11 @@ public class ChatController : MonoBehaviour
 
     IEnumerator TextPractice()
     {
-        yield return StartCoroutine(NormalChat("스토리", "오랜 세월 해적들 사이에 전해 내려오던 신의 유적이라 불리는 지도가 드디어 발견되었다."));
+        yield return StartCoroutine(NormalChat("스토리", "오랜 세월 해적들 사이에 전해 내려오던 <color=#FFFF00>신의 유적</color>이라 불리는 <color=#FFFF00>지도</color>가 드디어 발견되었다."));
         yield return StartCoroutine(NormalChat("스토리", "그 지도가 이끄는 곳은 바로 거대한 바다 위에 떠 있는 신비로운 탑이었다. "));
         yield return StartCoroutine(NormalChat("스토리", "탐욕에 눈이 먼 해적들은 망설임 없이 탑으로 향했지만,  "));
         yield return StartCoroutine(NormalChat("스토리", "그들은 예상치 못한 존재와 마주하게 된다.  "));
-        yield return StartCoroutine(NormalChat("스토리", "바로 그 탑은 전설적인 대마법사가 은둔하며 살고 있는 곳이었던 것이다.  "));
+        yield return StartCoroutine(NormalChat("스토리", "바로 그 탑은 전설적인 <color=#FFFF00>대마법사</color>가 은둔하며 살고 있는 곳이었던 것이다.  "));
         yield return StartCoroutine(NormalChat("스토리", "해적들이 상륙하던 순간,"));
         yield return StartCoroutine(NormalChat("스토리", "자신의 영역에 침입한 이들을 발견한 그는 그들을 소탕하기로 한다  "));
     }
