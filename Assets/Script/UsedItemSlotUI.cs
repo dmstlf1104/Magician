c3c543905ad5d857b5cc51087100ce9bf961ab52
박지훈 <150890216+art-riel@users.noreply.github.com>
2024-02-16 10:39:17 +0900

    fix
    필요없는 스크립트 제거

--- Assets/Script/UsedItemSlotUI.cs
+++ Assets/Script/UsedItemSlotUI.cs
@@ -27,8 +27,4 @@ public class UsedItemSoltUI : MonoBehaviour
         UnitManagement.instance.SelectItem(index);
     }
 
-    public void OnIndex()
-    {
-        InGameItem.instance.SetIndex(index);
-    }
 }
